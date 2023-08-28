5f32fde4563b4ed11f4b9dd7e7315826db1dd8f9
Oleg Brusnik <oleg.brusnik.93@mail.ru>
2023-08-28 20:38:47 +0500

    exercize 1 2 done

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("button:not(:only-of-type)");
+
+const themeBtn = document.getElementById("theme");
 
 const divs = document.querySelectorAll(".content");
 
-function resetButtons(buttons, index) {
+function switchBtnActiveState(buttons, index) {
   buttons.forEach((b) => {
     b.classList.remove("active");
   });
   buttons[index].classList.add("active");
 }
 
-function renderDivs(divs, index) {
+function switchDivActiveState(divs, index) {
   divs.forEach((div) => {
     div.classList.remove("active");
   });
-  divs[index].classList.add("active")
+  divs[index].classList.add("active");
 }
 
 buttons.forEach((button, index) => {
   button.addEventListener("click", (e) => {
-    resetButtons(buttons, index);
-    renderDivs(divs, index);
+    switchBtnActiveState(buttons, index);
+    switchDivActiveState(divs, index);
   });
 });
+
+function changeTheme() {
+  let dark = false;
+  themeBtn.addEventListener("click", (e) => {
+    themeBtn.classList.toggle("light-theme");
+    themeBtn.classList.toggle("dark-theme");
+    if (dark === false) {
+      dark = true;
+      document.body.classList.add("dark");
+    } else {
+      dark = false;
+      document.body.classList.remove("dark");
+    }
+  });
+}
+
+changeTheme();
